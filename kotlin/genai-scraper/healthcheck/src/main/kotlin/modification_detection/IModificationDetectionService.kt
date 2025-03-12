b61208aba372f656898c1ecd4cd033670aab2821
Joao Cardoso <joao.cardoso@challenge.pt>
2025-03-12 14:38:05 +0000

    feat: getMissingElements now compares two html snapshots to find missing elements

--- kotlin/genai-scraper/healthcheck/src/main/kotlin/modification_detection/IModificationDetectionService.kt
+++ kotlin/genai-scraper/healthcheck/src/main/kotlin/modification_detection/IModificationDetectionService.kt
@@ -4,5 +4,6 @@ import classes.data.Element
 import classes.service_model.Modification
 
 interface IModificationDetectionService {
+    suspend fun getMissingElements(previousHTMLState: String, newHTMLState: String): List<Element>
     suspend fun getModification(modifiedElement: Element, newElements: List<Element>): Modification
 }
--- kotlin/genai-scraper/healthcheck/src/main/kotlin/modification_detection/ModificationDetectionService.kt
+++ kotlin/genai-scraper/healthcheck/src/main/kotlin/modification_detection/ModificationDetectionService.kt
@@ -6,6 +6,7 @@ import classes.service_model.Modification
 import domain.classes.LLM
 import domain.http.ollama.requests.OllamaGenerateRequest
 import domain.modification.requests.ModificationRequest
+import domain.prompts.GET_MISSING_ELEMENTS_PROMPT
 import domain.prompts.GET_MODIFICATION_PROMPT
 import kotlinx.coroutines.runBlocking
 import kotlinx.serialization.builtins.ListSerializer
@@ -17,6 +18,23 @@ import ollama.OllamaClient
 class ModificationDetectionService(
     private val llmClient: ILLMClient,
 ) : IModificationDetectionService {
+    override suspend fun getMissingElements(previousHTMLState: String, newHTMLState: String): List<Element> {
+        val missingElementsRequest = OllamaGenerateRequest(
+            model = LLM.Mistral7B.modelName,
+            system = GET_MISSING_ELEMENTS_PROMPT,
+            prompt = """
+                BEFORE:
+                $previousHTMLState
+                AFTER:
+                $newHTMLState
+            """.trimIndent(),
+            stream = false,
+            raw = false
+        )
+        val missingElementsResponseJson = llmClient.generate(missingElementsRequest).response
+        return Json.decodeFromString<List<Element>>(missingElementsResponseJson)
+    }
+
     override suspend fun getModification(modifiedElement: Element, newElements: List<Element>): Modification {
         val modifiedElementJson = Json.encodeToString(Element.serializer(), modifiedElement)
         val newElementsJson = Json.encodeToString(ListSerializer(Element.serializer()), newElements)
@@ -43,13 +61,35 @@ fun main() {
         val httpClient = OkHttpClient()
         val llmClient = OllamaClient(httpClient)
         val service = ModificationDetectionService(llmClient)
-        val modifiedElement = Element("BUTTON", "button.btn-xs", "Sign up")
+        /*val modifiedElement = Element("BUTTON", "button.btn-xs", "Sign up")
         val newElements = listOf(
             Element(ElementTypes.TEXTAREA.name, "textarea.comments", "Comments"),
             Element(ElementTypes.BUTTON.name, "link.link-register", "Register"),
             Element(ElementTypes.HEADING.name, "h2.h2-title", "Register an account.")
         )
         val modification = service.getModification(modifiedElement, newElements)
-        println(modification)
+        println(modification)*/
+        val previousHtml = """
+            <!DOCTYPE html>
+            <html>
+            <head><title>Example</title></head>
+            <body>
+                <button id="show-p">Click to reveal</button>
+                <p id="hidden-p" style="display: none;">Hello, bro!</p>
+            </body>
+            </html>
+        """.trimIndent()
+        val newHtml = """
+            <!DOCTYPE html>
+            <html>
+            <head><title>Example</title></head>
+            <body>
+                <div id="reveal-message-div">Press this</div>
+                <p id="hidden-text" style="visibility: hidden;">Hello, brother!</p>
+            </body>
+            </html>
+        """.trimIndent()
+        val missingElements = service.getMissingElements(previousHtml, newHtml)
+        println(missingElements)
     }
 }
