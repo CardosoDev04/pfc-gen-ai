a0e03b3430b8a61f6c79bfe97a0698264fedcbba
HarvyDev <franciscolbantunes@gmail.com>
2025-03-17 09:55:43 +0000

    file io WIP

--- kotlin/genai-scraper/common_services/src/main/kotlin/persistence/PersistenceService.kt
+++ kotlin/genai-scraper/common_services/src/main/kotlin/persistence/PersistenceService.kt
@@ -3,6 +3,6 @@ package persistence
 import classes.llm.Model
 
 interface PersistenceService {
-    fun write(model: Model, fileContent: String)
+    fun write(modelName: String, fileContent: String)
     fun read(scraperPath: String): String
 }
--- kotlin/genai-scraper/common_services/src/main/kotlin/persistence/implementations/FilePersistenceService.kt
+++ kotlin/genai-scraper/common_services/src/main/kotlin/persistence/implementations/FilePersistenceService.kt
@@ -1,15 +1,30 @@
 package persistence.implementations
 
+import classes.llm.LLM
 import classes.llm.Model
 import persistence.PersistenceService
 import java.io.File
 
-class FilePersistenceService : PersistenceService {
-    override fun write(model: Model, fileContent: String) {
-        TODO("Not yet implemented")
+class FilePersistenceService(
+    private val resultsBaseDir: String = System.getProperty("user.dir") + "/results"
+) : PersistenceService {
+    override fun write(modelName: String, fileContent: String) {
+        val latestDirectory = File("${resultsBaseDir}/${modelName}/latest")
+        if (latestDirectory.exists() && latestDirectory.isDirectory) {
+            // TODO("Cut the contents from this directory and paste them into the appropriate timestamp")
+        } else {
+            // TODO("If the directory does not exist create it")
+        }
+
+        // TODO("Write the file content to the latest directory")
     }
 
     override fun read(scraperPath: String): String {
         return File(scraperPath).readText()
     }
+}
+
+fun main() {
+    val fps = FilePersistenceService()
+    fps.write(LLM.Mistral7B.modelName, "Hello World!")
 }
