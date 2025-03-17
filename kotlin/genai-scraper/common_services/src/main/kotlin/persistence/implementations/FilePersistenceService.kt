5a36f98f9f1d0c57f5f436fb8abf07a2b8a9d355
HarvyDev <franciscolbantunes@gmail.com>
2025-03-17 12:58:00 +0000

    FilePersistenceService implemented

--- kotlin/genai-scraper/common_services/src/main/kotlin/persistence/implementations/FilePersistenceService.kt
+++ kotlin/genai-scraper/common_services/src/main/kotlin/persistence/implementations/FilePersistenceService.kt
@@ -1,30 +1,56 @@
 package persistence.implementations
 
-import classes.llm.LLM
-import classes.llm.Model
 import persistence.PersistenceService
 import java.io.File
+import java.time.Instant
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
 
 class FilePersistenceService(
     private val resultsBaseDir: String = System.getProperty("user.dir") + "/results"
 ) : PersistenceService {
-    override fun write(modelName: String, fileContent: String) {
+    override fun write(modelName: String, fileName: String, fileContent: String) {
         val latestDirectory = File("${resultsBaseDir}/${modelName}/latest")
         if (latestDirectory.exists() && latestDirectory.isDirectory) {
-            // TODO("Cut the contents from this directory and paste them into the appropriate timestamp")
+            copyDirectory(latestDirectory, File("${resultsBaseDir}/${modelName}/${getCurrentTimestamp()}"))
         } else {
-            // TODO("If the directory does not exist create it")
+            latestDirectory.mkdirs()
         }
 
-        // TODO("Write the file content to the latest directory")
+        val file = File(latestDirectory, fileName)
+        file.createNewFile()
+        file.writeText(fileContent)
     }
 
     override fun read(scraperPath: String): String {
         return File(scraperPath).readText()
     }
-}
 
-fun main() {
-    val fps = FilePersistenceService()
-    fps.write(LLM.Mistral7B.modelName, "Hello World!")
+    private fun copyDirectory(sourceDir: File, destDir: File) {
+        val sourceDirFiles = sourceDir.listFiles()
+
+        if (!destDir.exists() && sourceDirFiles?.size != 0) {
+            destDir.mkdirs()
+        }
+
+        sourceDirFiles?.forEach { file ->
+            val destFile = File(destDir, file.name)
+            if (file.isDirectory()) {
+                copyDirectory(file, destFile)
+                file.delete()
+            } else {
+                file.inputStream().use { input ->
+                    destFile.outputStream().use { output ->
+                        input.copyTo(output)
+                    }
+                }
+                file.delete()
+            }
+        }
+    }
+
+    private fun getCurrentTimestamp(): String {
+        val formatter = DateTimeFormatter.ofPattern("yyyyMMdd_HHmmSS").withZone(ZoneId.systemDefault())
+        return formatter.format(Instant.now())
+    }
 }
